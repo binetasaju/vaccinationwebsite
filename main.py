354ea760a2cac2a58f3bde6e4adf6dc015b00bf4
binetasaju <binetasaju@gmail.com>
2022-10-04 23:32:35 +0530

    Initial version

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)#__name__ is the saved file name
+
+@app.route("/")#
+def websitedata():
+    print("got request")
+    return render_template("index.html")
+
+@app.route("/about")#
+def aboutdata():
+    print("got request")
+    return render_template("about.html")
+if __name__=="__main__":
+    app.run(debug=True, port=5000)
+    print("running")
